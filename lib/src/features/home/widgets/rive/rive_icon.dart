b0e72b85e94f6c7223d3e64776637b86f7507b1f
Koliy82 <sokol070804@mail.ru>
2024-05-12 06:35:01 +0300

    refactor(Drawer Button): change link from telegram to email support

--- lib/src/features/home/widgets/rive/rive_icon.dart
+++ lib/src/features/home/widgets/rive/rive_icon.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mine_profile/src/core/extensions/context_extension.dart';
 import 'package:mine_profile/src/features/home/models/rive_icons.dart';
 import 'package:rive/rive.dart';
 
@@ -15,21 +16,31 @@ class RiveIcon extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: height ?? 48,
-      width: width ?? 48,
-      child: RiveAnimation.asset(
-        'assets/rive/animated_icons.riv',
-        artboard: icon.name,
-        onInit: (p0) {
-          controller = StateMachineController.fromArtboard(
-            p0,
-            'State Machine 1',
-          )!;
-          p0.addController(controller!);
-          onAnimate = controller!.findInput<bool>('HOVERED?') as SMIBool;
-          onAnimate?.value = isActive;
-        },
+    var theme = context.theme;
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: theme.brightness == Brightness.dark
+            ? Colors.black12
+            : Colors.black87,
+        border: Border.all(color: theme.colorScheme.surface),
+      ),
+      child: SizedBox(
+        height: height ?? 48,
+        width: width ?? 48,
+        child: RiveAnimation.asset(
+          'assets/rive/animated_icons.riv',
+          artboard: icon.name,
+          onInit: (p0) {
+            controller = StateMachineController.fromArtboard(
+              p0,
+              'State Machine 1',
+            )!;
+            p0.addController(controller!);
+            onAnimate = controller!.findInput<bool>('HOVERED?') as SMIBool;
+            onAnimate?.value = isActive;
+          },
+        ),
       ),
     );
   }
